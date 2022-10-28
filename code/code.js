194b52df4a2913f9960778c8ff63b7743f6b6817
Diegojnb <diegojnb@gmail.com>
2022-10-28 14:49:10 +0200

    added code if phone is rotated

--- code/code.js
+++ code/code.js
@@ -7,3 +7,16 @@ jQuery(document).ready(function() {
         }
     });
 });
+
+
+function myNavFunc(){
+    var dest = "40.923157, -3.560784"
+
+    if( (navigator.platform.indexOf("iPhone") != -1) 
+        || (navigator.platform.indexOf("iPod") != -1)
+        || (navigator.platform.indexOf("iPad") != -1)){
+        window.open("maps://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + dest);
+    }else{
+        window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + dest);
+    }
+}
