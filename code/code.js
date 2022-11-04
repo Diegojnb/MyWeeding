47a8f8a30a5d1f64a99b3e902812aa9f709b2f41
Diegojnb <diegojnb@gmail.com>
2022-11-04 21:29:47 +0100

    load only main page

--- code/code.js
+++ code/code.js
@@ -6,8 +6,25 @@ jQuery(document).ready(function() {
             $(this).html('&#9776');
         }
     });
+
+    
 });
 
+$(document).ready(function(){
+    $("#principal").load("main.html");
+	$("#index").click(function(){
+		$("#principal").load("main.html");
+	});
+	$("#schedule").click(function(){
+		$("#principal").load("schedule.html");
+	});
+    $("#location").click(function(){
+		$("#principal").load("location.html");
+	});
+    $("#contact").click(function(){
+		$("#principal").load("contact.html");
+	});
+});
 
 function myNavFunc(){
     var dest = "40.923157, -3.560784"
@@ -20,4 +37,3 @@ function myNavFunc(){
         window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + dest);
     }
 }
-
---
+++ renombrarFicheros.py
@@ -0,0 +1,15 @@
+import os
+
+# folder path
+dir_path = '.\\images\\carrousel'
+
+# list to store files
+secuencial = 1
+
+# Iterate directory
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    print(path)
+    
+    os.rename(dir_path + "\\" + path,dir_path + "\\" + str(secuencial).zfill(3) + ".jpg")
+    secuencial+=1
